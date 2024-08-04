e71444f9f710ee08aad0eb38d72c1d56d40d8524
Baptiste Nautré <nautre.baptiste@hotmail.com>
2024-08-04 20:47:41 +0200

    changes

--- src/Components/ExperienceSection.jsx
+++ src/Components/ExperienceSection.jsx
@@ -1,16 +1,30 @@
 import SectionTitle from "./SectionTitle";
-import {faSuitcase} from "@fortawesome/free-solid-svg-icons";
+import { faSuitcase } from "@fortawesome/free-solid-svg-icons";
 
 export default function ExperienceSection() {
     return (
         <div className="Experience_Section" id={"experience"}>
-            <SectionTitle label={"Experience"} SectionIcon={faSuitcase}/>
+            <SectionTitle label={"Experience"} SectionIcon={faSuitcase} />
             <main>
+                <section>
+                    <p>09/2023</p>
+                    <div>
+                        <h1>Web Developer Alternating</h1>
+                        <h6 onClick={() => window.location.href = "https://www.monstock.net/"}>Monstock</h6>
+                    </div>
+                </section>
+                <section>
+                    <p>09/2023 - 12/2025</p>
+                    <div>
+                        <h1>Mastere Developper Full Stack</h1>
+                        <h6 onClick={() => window.location.href = "https://www.livecampus.fr/formations/mastere-developpeur-web-full-stack"}></h6>
+                    </div>
+                </section>
                 <section>
                     <p>04/2023 - 06/2023 </p>
                     <div>
                         <h1>Web Developer Internship</h1>
-                        <h6>Un Zero Un Agency</h6>
+                        <h6 onClick={() => window.location.href = "https://www.un-zero-un.fr/"}>Un Zero Un Agency</h6>
                     </div>
                 </section>
                 <section>
